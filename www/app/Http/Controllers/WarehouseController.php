c7022c9fb4f1752c66e9833cd676c55244892dc5
cesar_ciau <cesarfhs@gmail.com>
2021-12-29 00:37:41 -0500

    data_store

---
+++ www/app/Http/Controllers/WarehouseController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class WarehouseController extends Controller
+{
+    private function getVehicleList() {
+
+    }
+
+    private function storeVehicle() {
+
+    }
+
+    private function updateVehicle() {
+
+    }
+    private function deleteVehicle() {
+
+    }
+}
