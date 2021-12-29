d88b15918ea5bed34fb122b1d0e929ffc6e322a1
cesar_ciau <cesarfhs@gmail.com>
2021-12-29 16:30:00 -0500

    new views

---
+++ app_client/src/components/vehicle/v-list.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col>
+        <v-btn color="success" class="mr-4">
+          <v-icon left>
+            mdi-plus
+          </v-icon>
+          Nuevo
+        </v-btn>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="4" v-for="n in 4" :key="n">
+        <v-card elevation="2">
+          <v-card-title>Cafe Badilico</v-card-title>
+          <v-card-subtitle>
+            Tipo Vehiculo
+          </v-card-subtitle>
+          <v-img height="250" src="https://random.imagecdn.app/500/300"></v-img>
+          <v-card-text>
+            description
+          </v-card-text>
+          <v-card-actions>
+            <v-btn color="primary" outlined>
+              Editar
+              <v-icon left>
+                mdi-pencil
+              </v-icon>
+            </v-btn>
+
+            <v-btn color="error" outlined>
+              Eliminar
+              <v-icon left>
+                mdi-delete
+              </v-icon>
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+<script>
+export default {
+  name: 'VehicleList',
+  mounted () {
+      
+  }
+}
+</script>
--- app_client/src/views/Home.vue
+++ app_client/src/views/Home.vue
@@ -5,35 +5,16 @@
         <h1 class="text-center">Listado de Vehiculos</h1>
       </v-col>
     </v-row>
-    <v-row>
-        <v-col cols="4" v-for="n in 4" :key="n">
-          <v-card elevation="2">
-            <v-card-title>Cafe Badilico</v-card-title>
-            <v-card-subtitle>
-              Tipo Vehiculo
-            </v-card-subtitle>
-            <v-card-text>
-              description
-            </v-card-text>
-            <v-card-actions>
-              <v-btn color="green">
-                Editar
-              </v-btn>
-
-              <v-btn color="red">
-                Elimar
-              </v-btn>
-            </v-card-actions>
-          </v-card>
-        </v-col>
-    </v-row>
+    <List/>
   </v-container>
 </template>
-
 <script>
 // @ is an alias to /src
-
+import List from '@/components/vehicle/v-list'
 export default {
-  name: 'Home'
+  name: 'Home',
+  components: {
+    List
+  }
 }
 </script>
