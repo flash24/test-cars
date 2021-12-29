5af5497aab8d73632b18e9fbc6cd3a46c7da92a6
cesar_ciau <cesarfhs@gmail.com>
2021-12-29 15:34:23 -0500

    v cards list

--- app_client/src/main.js
+++ app_client/src/main.js
@@ -6,7 +6,7 @@ import Vue from 'vue'
 import App from './views/App.vue'
 import store from './store'
 import router from './router'
-import 'font-awesome/css/font-awesome.min.css'
+// import 'font-awesome/css/font-awesome.min.css'
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
 import VueSweetalert2 from 'vue-sweetalert2';
 import 'sweetalert2/dist/sweetalert2.min.css';
--- app_client/src/views/Home.vue
+++ app_client/src/views/Home.vue
@@ -6,30 +6,26 @@
       </v-col>
     </v-row>
     <v-row>
-      <v-col cols="4">
-        <v-card elevation="2">
-          <v-card-title>Cafe Badilico</v-card-title>
-          <v-card-subtitle>
-            Tipo Vehiculo
-          </v-card-subtitle>
-        </v-card>
-      </v-col>
-      <v-col cols="4">
-        <v-card elevation="2">
-          <v-card-title>Cafe Badilico</v-card-title>
-          <v-card-subtitle>
-            Tipo Moto
-          </v-card-subtitle>
-        </v-card>
-      </v-col>
-      <v-col cols="4">
-        <v-card elevation="2">
-          <v-card-title>Cafe Badilico</v-card-title>
-          <v-card-subtitle>
-            Tipo Vehiculo
-          </v-card-subtitle>
-        </v-card>
-      </v-col>
+        <v-col cols="4" v-for="n in 4" :key="n">
+          <v-card elevation="2">
+            <v-card-title>Cafe Badilico</v-card-title>
+            <v-card-subtitle>
+              Tipo Vehiculo
+            </v-card-subtitle>
+            <v-card-text>
+              description
+            </v-card-text>
+            <v-card-actions>
+              <v-btn color="green">
+                Editar
+              </v-btn>
+
+              <v-btn color="red">
+                Elimar
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
     </v-row>
   </v-container>
 </template>
