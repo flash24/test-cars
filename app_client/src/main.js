3844a1fe641e371cf2921af7dc48dc4ee34f0fff
cesar_ciau <cesarfhs@gmail.com>
2021-12-29 08:38:48 -0500

    vuetify

--- app_client/src/main.js
+++ app_client/src/main.js
@@ -1,12 +1,17 @@
+require('./bootstrap');
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
 Vue.config.productionTip = false
+Vue.use(Vuetify)
 
 new Vue({
   router,
+  vuetify: new Vuetify(),
   store,
   render: h => h(App)
 }).$mount('#app')
